ce13a1d916ba8be42828f0bd866b1d98019e4dcd
Twoje imięStaszekgit config --global user.name Staszekgit config --global user.name Twoje <stanislawko1981@gmail.com>
2016-05-10 11:44:49 +0200

    menu show

--- slider2.js
+++ slider2.js
@@ -1,16 +1,33 @@
 $(function() {
 
+     var pauseTime = 8000;
+
      $('#slider .pagination li').click(function (e) {
           e.preventDefault();
-          $('.box').toggleClass('active'); //włączenie/wyłączenie klasy
+          //po kliknięciu czyścimy timer
+          clearTimeout(timer);
+
           var $this = $(this);
           var $li = $this.parent().find('li');
           var index = $li.index($this);
 
-          $('#main .slide').removeClass('active').eq(index).addClass('active');
+          $('#slider .slide').removeClass('active').eq(index).addClass('active');
 
           $li.removeClass('active');
           $this.addClass('active');
+
+          //włączamy timer do kolejnego kliknięcia
+          timer = setTimeout(function() {
+               var index = $li.index($li.parent().find('.active'));
+               var clickLi = (index>=$li.length-1)? 0 : ++index;
+
+               $('#slider .pagination li').eq(clickLi).click();
+          }, pauseTime);
      });
 
-})
+     //po wejściu na stronę odpalamy pierwszy timer, który kliknie na drugi element paginacji
+     var timer = setTimeout(function() {
+          $('.pagination li').eq(1).click();
+     }, pauseTime);
+
+});
